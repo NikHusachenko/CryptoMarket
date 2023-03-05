c7a48b04c1828c9131fe736173005ddc68786dca
NikHusachenko <nikgusachenko@gmail.com>
2023-03-05 13:33:40 +0200

    Добавьте файлы проекта.

---
+++ CryptoMarket.Desktop/Program.cs
@@ -0,0 +1,12 @@
+namespace CryptoMarket.Desktop
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
