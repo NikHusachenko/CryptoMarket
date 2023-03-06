9f3c936f693f9dec45262b7251e234eb17610cd1
NikHusachenko <nikgusachenko@gmail.com>
2023-03-06 21:04:06 +0200

    Append .Services project. Create ApplicationDbContext

---
+++ CryptoMarket.Common/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace CryptoMarket.Common
+{
+    public class Constants
+    {
+        public const string DEFAULT_CONNECTION = "Data Source=localhost;Database=DocDb;Trusted_Connection=True;Encrypt=false";
+    }
+}
---
+++ CryptoMarket.EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CryptoMarket.Common;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace CryptoMarket.EntityFramework
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+            Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Constants.DEFAULT_CONNECTION);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
