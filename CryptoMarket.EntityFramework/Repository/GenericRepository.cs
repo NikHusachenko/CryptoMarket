d36c8d05612b502af2c4e4ddadca0d7fdb2e9d7c
NikHusachenko <nikgusachenko@gmail.com>
2023-03-10 17:08:07 +0200

    Implement GenericRepository

--- CryptoMarket.EntityFramework/Repository/GenericRepository.cs
+++ CryptoMarket.EntityFramework/Repository/GenericRepository.cs
@@ -1,6 +1,68 @@
-﻿namespace CryptoMarket.EntityFramework.Repository
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CryptoMarket.EntityFramework.Repository
 {
-    public class GenericRepository : IGenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : DbSet<T>
     {
+        private readonly ApplicationDbContext _context;
+        public DbSet<T> Table { get; }
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            Table = _context.Set<T>();
+        }
+
+        public async Task<bool> Create(T entity)
+        {
+            try
+            {
+                await Table.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex) 
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> Delete(T entity)
+        {
+            try
+            {
+                Table.Remove(entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<ICollection<T>> GetAll()
+        {
+            return await Table.ToListAsync();
+        }
+
+        public async Task<T> GetById(long id)
+        {
+            return await Table.FindAsync(id);
+        }
+
+        public async Task<bool> Update(T entity)
+        {
+            try
+            {
+                Table.Update(entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
--- CryptoMarket.EntityFramework/Repository/IGenericRepository.cs
+++ CryptoMarket.EntityFramework/Repository/IGenericRepository.cs
@@ -1,7 +1,15 @@
-﻿namespace CryptoMarket.EntityFramework.Repository
-{
-    public interface IGenericRepository
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace CryptoMarket.EntityFramework.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        DbSet<T> Table { get; }
+
+        Task<bool> Create(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+        Task<T> GetById(long id);
+        Task<ICollection<T>> GetAll();
     }
 }
