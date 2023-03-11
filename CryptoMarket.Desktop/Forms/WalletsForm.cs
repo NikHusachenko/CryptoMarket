75c8a7f224b779d045804d8ad9dc9b63f868efbb
Oleg <olegredko45@gmail.com>
2023-03-11 20:27:14 +0200

    Add methods and fields for WalletsForm

--- CryptoMarket.Desktop/Forms/WalletsForm.cs
+++ CryptoMarket.Desktop/Forms/WalletsForm.cs
@@ -3,41 +3,66 @@
 	public partial class WalletsForm : Form
 	{
 		static FlowLayoutPanel _walletsFlowLayotPanel;
-		int _walletsCount = 1; // Тут буде _walletsCount буде дорівнювати кількості Гаманців користувача
+	    static int _walletsCount = 5; // Тут _walletsCount буде дорівнювати кількості Гаманців користувача
 		const int MaxNumWallets = 4;
+
 		public WalletsForm()
 		{
 			InitializeComponent();
 			_walletsFlowLayotPanel = WalletsFlowLayoutPanel;
-
-
+			AddExistingWallets();
 		}
-		
+
+		private static void AddExistingWallets()
+		{
+			if (_walletsCount == 1)
+			{
+				return;
+			}
+			else
+			{
+				for(int i=1; i <= _walletsCount - 1; i++)
+				{
+					_walletsFlowLayotPanel.Controls.Add(CreateWallet($"Wallet {i}"));
+				}
+			}
+		}
+
+		private static Button CreateWallet(string walletName)
+		{
+			if (_walletsFlowLayotPanel.Controls.ContainsKey(walletName) && _walletsCount != 1)
+			{
+				MessageBox.Show("Error");
+				return null;
+			}
+			else
+			{
+				Button button = new Button()
+				{
+					Font = new System.Drawing.Font("Segoe UI", 47.76237F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point),
+					Location = new System.Drawing.Point(3, 3),
+					Name = walletName,
+					Size = new System.Drawing.Size(388, 329),
+					Text = walletName,
+				};
+				return button;
+			}
+		}
 
 		private void button3_Click(object sender, EventArgs e)
 		{
 			if(_walletsCount >= MaxNumWallets ) 
 			{
 				button3.Hide();
-				//return;
    			}
 			string walletName = "Wallet " + _walletsCount.ToString();
-			if (_walletsFlowLayotPanel.Controls.ContainsKey(walletName))
-			{
-				return;
-			}
 
-			Button button = new Button()
-			{
-				Font = new System.Drawing.Font("Segoe UI", 47.76237F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point),
-			    Location = new System.Drawing.Point(3, 3),
-			    Name = walletName,
-			    Size = new System.Drawing.Size(388, 329),
-			    Text = "Wallet_" + _walletsCount.ToString(),
-		   };
+		    Button button = CreateWallet(walletName);
 			_walletsFlowLayotPanel.Controls.Add(button);
 			_walletsCount++;
 			
 		}
+
+		
 	}
 }
