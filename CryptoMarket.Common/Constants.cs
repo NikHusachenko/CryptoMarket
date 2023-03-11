74908d98958f9fb845032ce7ed9efbd76a896025
NikHusachenko <nikgusachenko@gmail.com>
2023-03-11 11:31:54 +0200

    Add implementation of SendGrid Twillio API

--- CryptoMarket.Common/Constants.cs
+++ CryptoMarket.Common/Constants.cs
@@ -1,5 +1,13 @@
 ﻿namespace CryptoMarket.Common
 {
+    public class SendGridConfiguration
+    {
+        public const string EMAIL_FROM = "kkolyann99kkolyann@gmail.com";
+        public const string NAME_FROM = "CryptoMarket";
+        public const string API_KEY = "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";
+        public const string EMAIL_TITLE = "You have message from CryptoMarket!";
+    }
+
     public class DbConfiguration
     {
         public const string DEFAULT_CONNECTION = "Data Source=localhost;Database=CryptoMarketDb;Trusted_Connection=True;Encrypt=false";
---
+++ CryptoMarket.Services/EmailService/IEmailSenderService.cs
@@ -0,0 +1,9 @@
+﻿using CryptoMarket.Services.Response;
+
+namespace CryptoMarket.Services.EmailService
+{
+    public interface IEmailSenderService
+    {
+        Task<ResponseService> SendEmail(string emailTo, string nameTo, string message);
+    }
+}
