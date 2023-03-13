a7e1bb8bde9c65243b52bf68ff7fe0f5d55728ca
Oleg <olegredko45@gmail.com>
2023-03-13 16:47:37 +0200

    add LoginForm, SignUpLabel_Click

--- CryptoMarket.Desktop/Forms/LoginForm.cs
+++ CryptoMarket.Desktop/Forms/LoginForm.cs
@@ -1,10 +1,89 @@
-﻿namespace CryptoMarket.Desktop.Forms
+﻿using CryptoMarket.Database.Entities;
+
+namespace CryptoMarket.Desktop.Forms
 {
     public partial class LoginForm : Form
     {
+		static List<UserEntity> userEntities;//here I will get all Users
         public LoginForm()
         {
             InitializeComponent();
+			userEntities= new List<UserEntity>();
         }
-    }
+		private void SignUpLabel_Click(object sender, EventArgs e)
+		{
+			RegistrationForm registrationForm = new RegistrationForm();
+			registrationForm.Show();
+			registrationForm.FormClosed += ShowThisForm;
+			this.Hide();
+		}
+
+		private void SignInLabel_Click(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrEmpty(LoginTextBox.Text))
+			{
+				if (!string.IsNullOrEmpty(PasswordTextBox.Text))
+				{
+					UserEntity userEntity = new UserEntity() { Login = "Oleg" , Email = "olegredko@gmail.com",CreatedOn = DateTime.Now,Password = "1"};
+					userEntities.Add(userEntity);
+					foreach (UserEntity user in userEntities)
+					{
+						if (LoginTextBox.Text == user.Login)
+						{
+								if (PasswordTextBox.Text == user.Password)
+								{
+									MarketForm market = new MarketForm(user);
+									market.Show();
+									market.FormClosed += CloseThisForm;
+									this.Hide();
+									break;
+								}
+								else
+								{
+								PasswordErrorLabel.Visible = true;
+								PasswordErrorLabel.Text = "Password is not correct";
+									//MessageBox.Show("Password is not correct");
+								}
+						}
+						else
+						{
+							LoginErrorLabel.Visible = true;
+							LoginErrorLabel.Text = $"<- Login is not correct";
+							//	MessageBox.Show("Login is not correct");
+						}
+					}
+				}
+				else
+				{
+					PasswordErrorLabel.Visible = true;
+					PasswordErrorLabel.Text = $"<- Password cannot be empty";
+					//	MessageBox.Show("Password cannot be empty!");
+				}
+			}
+			else
+			{
+				LoginErrorLabel.Visible = true;
+				LoginErrorLabel.Text = $"<- Login cannot be empty";
+				//	MessageBox.Show("Login cannot be empty!");
+			}
+		}
+		private void ShowThisForm(object sender, FormClosedEventArgs e)
+		{
+			this.Show();
+		}
+		private void CloseThisForm(object sender, FormClosedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void LoginTextBox_TextChanged(object sender, EventArgs e)
+		{
+			LoginErrorLabel.Visible = false;
+		}
+
+		private void PasswordTextBox_TextChanged(object sender, EventArgs e)
+		{
+			PasswordErrorLabel.Visible = false;
+		}
+	}
 }
