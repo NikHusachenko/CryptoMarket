363673d182ebea6f27f9029591f944741a48ce64
Swayze <kovaloff10@gmail.com>
2023-03-14 13:46:26 +0200

    Coins in database

---
+++ CryptoMarket.Database/Entities/CoinInfoEntity.cs
@@ -0,0 +1,24 @@
+﻿using CryptoMarket;
+
+namespace CryptoMarket.Database.Entities
+{
+    public class CoinInfoEntity
+    {
+        CoinInfoConfiguration coin = Co
+        public string Name
+        {
+            get
+            {
+                return ;
+            }
+            set
+            {
+                return 0;
+            }
+        }
+
+        public string Unit { get; set; }
+        public float Value { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ CryptoMarket.EntityFramework/Configurations/CoinInfoConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CryptoMarket.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CryptoMarket.EntityFramework.Configurations
+{
+    public class CoinInfoConfiguration : IEntityTypeConfiguration<CoinInfoEntity>
+    {
+        public void Configure(EntityTypeBuilder<CoinInfoEntity> builder)
+        {
+            builder.ToTable("Coins").HasKey(coin => coin.Unit);
+
+            builder.Property(user => user.Name).IsRequired(true);
+            builder.Property(user => user.Value).IsRequired(true);
+            builder.Property(user => user.Type).IsRequired(true);
+        }
+    }
+}
