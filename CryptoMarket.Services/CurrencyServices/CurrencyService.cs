d2725c687330e46086d2d9b70effbab97f7128d5
Oleg <olegredko45@gmail.com>
2023-03-19 19:42:27 +0200

    Add CurrencyServices

---
+++ CryptoMarket.Services/CurrencyServices/CurrencyService.cs
@@ -0,0 +1,38 @@
+﻿using CryptoMarket.Database.Entities;
+using CryptoMarket.EntityFramework.Repository;
+using Newtonsoft.Json;
+
+namespace CryptoMarket.Services.CurrencyServices
+{
+	public class CurrencyService : ICurrencyService
+	{
+		private readonly IGenericRepository<CurrencyEntity> _currencyRepository;
+
+		public CurrencyService(IGenericRepository<CurrencyEntity> currencyRepository)
+		{
+			_currencyRepository = currencyRepository;
+		}
+	
+		public void Create(CurrencyEntity currency)
+		{
+			_currencyRepository.Create(currency);
+		}
+
+		public async Task<CurrencyEntity> GetCurrencyByCurrencyIdAsync(string coinId)
+		{
+			try
+			{
+				var client = new HttpClient();
+				var message = await client.GetAsync($"https://api.coingecko.com/api/v3/coins/{coinId}");
+				message.EnsureSuccessStatusCode();
+				var context = await message.Content.ReadAsStringAsync();
+				var currency = JsonConvert.DeserializeObject<CurrencyEntity>(context);
+				return currency;
+			}
+			catch (Exception ex)
+			{
+				return null;
+			}
+		}
+	}
+}
---
+++ CryptoMarket.Services/CurrencyServices/ICurrencyService.cs
@@ -0,0 +1,11 @@
+﻿using CryptoMarket.Database.Entities;
+
+namespace CryptoMarket.Services.CurrencyServices
+{
+	public interface ICurrencyService
+	{
+		void Create(CurrencyEntity currency);
+		Task<CurrencyEntity> GetCurrencyByCurrencyIdAsync(string coinId);
+	}
+}
+
